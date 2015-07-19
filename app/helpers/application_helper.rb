77efa1fe86b9f49ffa0ae42e04d403df2be123f2
alex <alex@webit.in.ua>
2015-07-19 09:36:38 +0300

    added botstrap template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Подільська Товарна Біржа"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+ def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "alert-success"   # Green
+      when "error"
+        "alert-danger"    # Red
+      when "alert"
+        "alert-warning"   # Yellow
+      when "notice"
+        "alert-info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
 end
