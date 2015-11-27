4e093d47482fd8baa1c1f5d37380d686b5aa1fca
Alex <alex@webit.in.ua>
2015-11-27 12:25:15 +0200

    fixed syntax error

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,6 +3,6 @@ class Mailer < ApplicationMailer
   def feedback_email params
     email=Rails.application.config.birzha_settings[:email]
     @params = params
-    mail(to: email, subject: 'Повідомлення з сайту (зворотній зв\'язок)', from: "feedback@birhza.km.ua")
+    mail(to: email, subject: 'Повідомлення з сайту (зворотній зв\'язок)', from: "feedback@birzha.km.ua")
   end
 end
